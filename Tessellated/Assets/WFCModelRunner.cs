7f6f3595fe5a5bb6f530cd6f1f7f1b01f5291c47
Lukas Hartinger <58393218+Frazerus@users.noreply.github.com>
2024-06-12 17:34:01 +0200

    Better xml names and new test set

--- Tessellated/Assets/WFCModelRunner.cs
+++ Tessellated/Assets/WFCModelRunner.cs
@@ -34,7 +34,7 @@ public class WFCModelRunner : MonoBehaviour
         }
         else
         {
-            var model = new SimpleOctagonTessellationModel("octagon_singleSplit", 10, 10, false,
+            var model = new SimpleOctagonTessellationModel("octagon_Y_test", 10, 10, false,
                 OctagonTessellationModel.Heuristic.MRV, 2);
 
             var output = model.Run(seed, 10000);
